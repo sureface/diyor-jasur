11f8ac6b0c64cc75149240d7c773f5e688865a9c
sofame <cykamancer4@gmail.com>
2021-11-09 15:22:48 +0500

    validation page

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -6,13 +6,15 @@
             </div>
             <div class="card flex flex-col items-center bg-white shadow-lg p-14 rounded-lg">
                 <form class="flex flex-col w-full mb-4">
-                    <input type="text" name="login" placeholder="Ваш логин" class="h-14 w-full border border-gray-300 mb-3 px-4 rounded text-lg" required>
-                    <input type="password" name="password" placeholder="Ваш пароль" class=" h-14 w-full border border-gray-300 mb-7 px-4 rounded text-lg" required>
-                    <router-link to="/votepage">
-                        <button type="submit" class="text-center bg-blue-500 text-white h-14 w-full rounded font-semibold text-lg">Войти в кабинет</button>
-                    </router-link>
+                    <input v-model="inputLogin" type="text" name="login" placeholder="Ваш логин" :class="{'border-red-500' : inputLogin === ''}" class="h-14 w-full border  mb-3 px-4 rounded text-lg" required>
+                    <p class="text-red-500" v-if="!inputLogin">Введите Ваш Логин</p>
+                    <input v-model="inputPassword" type="password" name="password" placeholder="Ваш пароль" :class="{'border-red-500' : inputPassword === ''}" class=" h-14 w-full border  mb-7 px-4 rounded text-lg" required>
+                    <p class="text-red-500" v-if="!inputPassword">Введите Ваш Логин</p>
+                    <button @click.prevent="goToLogin" class="text-center bg-blue-500 text-white h-14 w-full rounded font-semibold text-lg">Войти в кабинет</button>
                 </form>
+
                 <a href="#" class="text-base text-gray-400 border-b border-gray-400 leading-none">Забыли пароль?</a>
+                <p class="text-red-500" v-if="trueData">Неправильный Логин или Пароль</p>
             </div>
             <div class="flex flex-col items-center justify-center">
                 <a href="#" class="mb-4"><img src="../assets/images/googlePlay.png" alt="oops sorry something wrong!"></a>
@@ -26,6 +28,23 @@
     import VoteList from "./VoteList.vue";
     import UploadPage from "./UploadPage.vue";
     export default {
+        data() {
+            return{
+                inputLogin: 'yugrusi',
+                inputPassword: 12345,
+                trueData: false
+            }
+
+        },
+        methods: {
+            goToLogin() {
+                if (this.inputLogin === 'yugrusi' && this.inputPassword === 12345) {
+                    this.$router.push('/votepage')
+                } else if (this.inputPassword !== '' && this.inputLogin !== '' && this.inputPassword !== 12345 && this.inputLogin !== 'yugrusi'){
+                    this.trueData = true
+                }
+            },
+        },
         components: {
             VoteList: VoteList,
             UploadPage: UploadPage,
@@ -33,6 +52,9 @@
     }
 </script>
 <style lang="scss">
+    .inputLogin{
+        border: 2px solid red;
+    }
     .wrapper{
         background: linear-gradient(to bottom, #EFF1F2 41%, #4484D8 0);
         .card{
